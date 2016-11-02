fe2f913f70569749e74ddd71194ba1ee54f3906a
dsaochin <dsaochin@hotmail.com>
2016-11-02 21:38:46 +0900

    add game result and death on profile

---
+++ public/js/game/result.js
@@ -0,0 +1,36 @@
+function goToResultScene(game){
+	//go to end screen
+	var endScene = new Scene();
+	//add scene environment
+	var bg = new Sprite(1920, 1080);
+	bg.image = game.assets[resultImg];
+	endScene.addChild(bg);
+	
+	//560,330
+	var labelList = new Label();
+	labelList.font = '100px Arial, Helvetica, sans-serif';
+	labelList.moveTo(560,330);
+	labelList.text = "Score <br>1<br>2<br>3<br>";
+	labelList.width = 480;
+	labelList.height = 730;
+	endScene.addChild(labelList);
+	var resultList = new Label();
+	resultList.font = '100px Arial, Helvetica, sans-serif';
+	resultList.moveTo(560,330);
+	resultList.textAlign  = "right";
+	resultList.text = "";
+	resultList.width = 820;
+	resultList.height = 730;
+	
+	var endScore =[5,100,2000,30000];
+	for(var i=0;i<endScore.length;i++){
+		//$("#userList").append(userObj[i].user.username + "<br>");
+		for(var j=5;j>(endScore[i].toString().length);j--){
+			resultList.text += "0";
+		}
+		resultList.text += endScore[i] + "<br>";
+	}
+	
+	endScene.addChild(resultList);
+	game.replaceScene(endScene);
+}
